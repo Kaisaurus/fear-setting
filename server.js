183d6e21ad3cd643873c2f2fd66522f8670c96a4
Egbert <m@egbert.co>
2019-02-25 09:27:53 +0100

    read current session on sessions page

--- server.js
+++ server.js
@@ -3,8 +3,23 @@ const app = express();
 const port = process.env.PORT || 5000;
 const proxy = require('http-proxy-middleware')
 
-app.get('/hello', (req, res) => {
-  res.send('Hello World!');
+app.get('/sessions', (req, res) => {
+  res.send(`
+    <html>
+      <head>
+      </head>
+      <body>
+        <script>
+          window.onload = function(){
+            let session = JSON.parse(localStorage.getItem('persist:root'))
+            let sessionChallenge = JSON.parse(session.challenge).challenge
+            document.getElementsByClassName('current_session')[0].innerText = sessionChallenge
+          }
+        </script>
+        <p>current session: <span class='current_session'></span></p>
+      </body>
+    </html>
+  `);
 })
 
 var apiProxy = proxy('/', {target: 'http://localhost:3000'});
@@ -12,4 +27,4 @@ app.use(apiProxy)
 
 app.listen(port, (req, res) => {
   console.log(`server listening on port: ${port}`)
- })
+})
